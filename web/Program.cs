9c364a0f08eb83e785a039ebb374c4f731eb327d
sofian <hadji22sofian@gmail.com>
2024-03-01 11:56:52 +0100

    added functionality that does not work

--- web/Program.cs
+++ web/Program.cs
@@ -1,6 +1,22 @@
+using hubs;
+using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.AspNetCore.Cors;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors(options => 
+{
+    options.AddPolicy("reactapp", builder => 
+    {
+        builder.WithOrigins("http://localhost:3000")
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
+    });
+});
 
 // Add services to the container.
+builder.Services.AddSignalR();
+builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -16,10 +32,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthorization();
 
+app.MapControllers();
 
-// .WithName()
-.WithOpenApi();
+app.MapHub<Chathub>(pattern:"/chat");
 
 app.Run();
-
--- web/obj/Debug/net8.0/web.AssemblyInfo.cs
+++ web/obj/Debug/net8.0/web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f5fea49149a72349a6e6a0c351445496ef47fb2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82e3f19bf6dbfcf118083a8944e20b5f16af6d26")]
 [assembly: System.Reflection.AssemblyProductAttribute("web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
